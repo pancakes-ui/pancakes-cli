3ea003973cd0f913536e71995df3624b4782dc16
Alisan <144158983+AZCPCF@users.noreply.github.com>
2025-10-01 16:49:11 +0330

    feat: add -h flag for help output

--- src/cli.ts
+++ src/cli.ts
@@ -1,17 +1,18 @@
 import { red } from 'colorette';
 import minimist from 'minimist';
+import { mainMenu } from './utils/main-menu';
 import { printLogo } from './utils/print-logo';
 import { checkForUpdate } from './utils/check-update';
-import { mainMenu } from './utils/main-menu';
 
 
-async function main() {
+export async function main() {
     const args = minimist(process.argv.slice(2));
 
     if (args.help || args.h) {
-        console.log('Usage: pancakes-cli');
+        console.log('Usage: pancakes-cli\nSee documentation at https://github.com/pancakes-ui/pancakes-cli/blob/main/README.md');
         process.exit(0);
     }
+
 
     if (args['check-update']) {
         printLogo(undefined);
@@ -22,7 +23,6 @@ async function main() {
 
     // Continue with menu after update check
     await mainMenu();
-
 }
 
 
