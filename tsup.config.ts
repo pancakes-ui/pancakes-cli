3c157022d2268e8426ed5c5307694bcda9921551
Alisan <144158983+AZCPCF@users.noreply.github.com>
2025-10-01 08:38:42 +0330

    chore(build): add tsup.config.ts for build configuration

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+// tsup.config.ts
+export default {
+    entry: ['src/cli.ts'],
+    format: ['cjs'],
+    dts: true,
+    clean: true,
+    splitting: false, // <- Try false to reduce overhead
+    sourcemap: false,
+    banner: {
+        js: '#!/usr/bin/env node',
+    },
+};
